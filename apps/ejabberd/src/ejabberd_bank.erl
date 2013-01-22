af36a19b2c7128e87aaf3312c152149827c8e013
Rafal Studnicki <rafal.studnicki@erlang-solutions.com>
2013-01-22 15:01:27 +0100

    Get rid of worker for starting bank pool

--- apps/ejabberd/src/ejabberd_bank.erl
+++ apps/ejabberd/src/ejabberd_bank.erl
@@ -5,7 +5,6 @@
 %%%===================================================================
 
 -module(ejabberd_bank).
--behaviour(gen_server).
 -include("ejabberd.hrl").
 
 %% Queries
@@ -35,22 +34,8 @@
          transaction/1,
          update_fun/6]).
 
-%% Worker starter
+%% Bank pool starter
 -export([start/0]).
-
-%% API
--export([start_link/1]).
-
-%% gen_server callbacks
--export([init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
-
--record(state, {host,
-                driver}).
 
 %%%===================================================================
 %%% Queries
@@ -199,126 +184,6 @@ set_privacy_list(Server, Username, Name, Items) ->
     bank:batch(Server, transaction(T)).
 
 %%%===================================================================
-%%% API
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
-start_link(Host) -> 
-    gen_server:start_link(?MODULE, [Host], []).
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
-init([Host]) ->
-    {Driver, Args, WorkerCount} = ejabberd_config:get_local_option({bank_server, Host}),
-    InitFun = fun() ->
-            {ok, State} = apply(Driver, connect, Args),
-            {ok, 0, 0, State2} = Driver:sql_query(<<"set names 'utf8'">>, State),
-            {ok, 0, 0, State3} = Driver:sql_query(<<"set session query_cache_type=1">>, State2),
-            NewState = lists:foldl(
-                    fun({Name, Query}, AccState) ->
-                            {ok, CurrState} = Driver:prepare(Name, Query, AccState),
-                            CurrState
-                    end, State3, prepared_statements()),
-            {ok, Driver, NewState}
-    end,
-
-    case bank:start_pool(Host, WorkerCount, [{init_fun, InitFun}]) of
-        ok ->
-            {ok, #state{host = Host,
-                        driver = Driver}};
-        Error ->
-            {stop, Error}
-    end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_info(_Info,  State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-    ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
@@ -415,32 +280,18 @@ prepared_statements() ->
         "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)">>}].
 
 %%%===================================================================
-%%% Worker starter
+%%% Bank pool starter
 %%%===================================================================
-
 start() ->
     lists:foreach(
         fun(Host) ->
                 case needs_bank(Host) of
-                    true -> start_bank(Host);
-                    _ -> ok
+                    true ->
+                        ok = init(Host);
+                    _ ->
+                        ok
                 end
         end, ?MYHOSTS).
-
-start_bank(Host) ->
-    WorkerName = gen_mod:get_module_proc(Host, ?MODULE),
-    ChildSpec = {WorkerName,
-                 {?MODULE, start_link, [Host]},
-                 transient,
-                 5000,
-                 worker,
-                 [?MODULE]},
-    case supervisor:start_child(ejabberd_sup, ChildSpec) of
-        {ok, _PID} -> ok;
-        Error ->
-            ?ERROR_MSG("Start of supervisor ~p failed:~n~p~nRetrying...~n", [WorkerName, Error]),
-            start_bank(Host)
-    end.
 
 needs_bank(Host) ->
     LHost = jlib:nameprep(Host),
@@ -449,6 +300,20 @@ needs_bank(Host) ->
         _ -> true
     end.
 
+init(Host) ->
+    {Driver, Args, WorkerCount} = ejabberd_config:get_local_option({bank_server, Host}),
+    InitFun = fun() ->
+            {ok, State} = apply(Driver, connect, Args),
+            {ok, 0, 0, State2} = Driver:sql_query(<<"set names 'utf8'">>, State),
+            {ok, 0, 0, State3} = Driver:sql_query(<<"set session query_cache_type=1">>, State2),
+            NewState = lists:foldl(
+                    fun({Name, Query}, AccState) ->
+                            {ok, CurrState} = Driver:prepare(Name, Query, AccState),
+                            CurrState
+                    end, State3, prepared_statements()),
+            {ok, Driver, NewState}
+    end,
+    bank:start_pool(Host, WorkerCount, [{init_fun, InitFun}]).
 
 %%%===================================================================
 %%% Helpers
